42eb04c62fdd203eea79e4192aa4699ce04dc9a7
ZenitsuPrjkt <saheuldarmian123@gmail.com>
2022-01-31 05:05:17 +0700

    initial commit

---
+++ elaina/README.md
@@ -0,0 +1,8 @@
+
+<p align="center">
+    <a href="https://www.codefactor.io/repository/github/ZenitsuPrjkt/ElainaRobot"><img src="https://www.codefactor.io/repository/github/ZenitsuPrjkt/ElainaRobot/badge?s=745e055fa302e5a4447c78cc7209bfcac5798841" alt="CodeFactor" /></a>
+    <a href="https://github.com/ZenitsuPrjkt/ElainaRobot"> <img src="https://img.shields.io/github/repo-size/ZenitsuPrjkt/ElainaRobot?color=orange&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/ZenitsuPrjkt/ElainaRobot/commits/prince"> <img src="https://img.shields.io/github/last-commit/ZenitsuPrjkt/ElainaRobot?color=brown&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://github.com/ZenitsuPrjkt/ElainaRobot/issues"> <img src="https://img.shields.io/github/issues/ZenitsuPrjkt/ElainaRobot?color=blueviolet&logo=github&logoColor=green&style=for-the-badge" /></a>
+    <a href="https://pypi.org/project/Telethon/"> <img src="https://img.shields.io/pypi/v/telethon?color=yellow&label=telethon&logo=python&logoColor=green&style=for-the-badge" /></a>
+</p>
---
+++ elaina/modules/alive.py
@@ -0,0 +1,45 @@
+# MIT License
+
+# Copyright (c) 2022 Zenitsu Prjkt™
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from elaina.events import register
+from elaina import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/7482398e772bd21a1f2bb.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Elaina Robot.** \n\n"
+  TEXT += "💠 **I'm Working Properly** \n\n"
+  TEXT += "💠 **My Master : [Zenitsu Prjkt](https://t.me/ZenitsuPrjkt)** \n\n"
+  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT)
